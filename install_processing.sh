05d5417c6d3ca7e564b1316ee5cbc3ad4ded438f
Krzem <krzem5.dev@gmail.com>
2021-04-22 20:02:44 +0200

    Push Update 04/22/2021, 20:02:40

--- install_processing.sh
+++ install_processing.sh
@@ -1,5 +1,5 @@
 [ -d __processing_core ]&&rm -rf __processing_core
 mkdir __processing_core
-git clone https://github.com/tmp-5y34hjweu/_tmp_rsrc_000.git
+git clone https://github.com/Krzem5-Rsrc/_tmp_rsrc_000.git
 cp -f _tmp_rsrc_000/lib/* __processing_core
 rm -rf _tmp_rsrc_000
